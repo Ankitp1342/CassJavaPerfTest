0291db0e3f5bdb7a528199e667231df12d4a2b0f
Ankitp1342 <ankitpatel@10.0.0.44>
2016-07-28 10:14:35 -0400

    refactor

--- src/main/java/perftest/PerfUtil.java
+++ src/main/java/perftest/PerfUtil.java
@@ -73,7 +73,7 @@ public class PerfUtil {
 		}
 		
 		logger.info("READ CONSISTENCY: " + readConsistency);
-		logger.info("WRITE CONSISTENCY: " + readConsistency);
+		logger.info("WRITE CONSISTENCY: " + writeConsistency);
 	}
 	public static void ratioReadKeys(){
 		
